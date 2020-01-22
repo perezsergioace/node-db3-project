9f5bbe0dc5046aff36bcb139738dce778d85f623
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-22 17:16:10 -0500

    wrote the add() method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,9 +2,14 @@ const db = require('../data/db-config');
 
 module.exports = {
     find,
+    findById,
 }
 
 function find() {
     return db.select("*").from("schemes");
+}
+
+function findById(schemeId) {
+    return db("schemes").where({id: schemeId})
 }
 
