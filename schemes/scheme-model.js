2b6f4c1af7c79dae8f83da3ea823dbb28e00fd5c
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-22 20:05:45 -0500

    wrote the findSteps() method, also updated some of the other methods to return the correct objects

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -6,26 +6,65 @@ module.exports = {
     add,
     update,
     remove,
+    findSteps,
 }
 
 function find() {
     return db.select("*").from("schemes");
 }
 
-function findById(schemeId) {
-    return db("schemes").where({id: schemeId});
+function findById(id) {
+    return db("schemes")
+        .where({ id })
+        .then(scheme => {
+            if (scheme) {
+                return scheme[0]
+            } else {
+                return null
+            }
+        });
 }
 
 function add(scheme) {
     return db("schemes")
         .insert(scheme)
-        .then(ids => ({id: ids[0]}))
+        .then(([id]) => {
+            return findById(id)
+        })
 }
 
-function update(schemeChanges, id) {
-    return db("schemes").where({ id }).update(schemeChanges);
+function update(changes, id) {
+    return db("schemes")
+        .where({ id })
+        .update(changes)
+        .then(updated => {
+            if (updated > 0) {
+                findById(id)
+            } else {
+                null
+            }
+            return findById(id)
+        })
 }
 
 function remove(id) {
-    return db("schemes").where({ id }).del();
+    console.log(id)
+    return findById(id)
+        .then(schemeId => {
+            if (schemeId) {
+                return db('schemes')
+                    .where({ id })
+                    .del()
+            } else {
+                return null
+            }
+        })
+}
+
+function findSteps(id) {
+    return db('steps as s')
+    .join('schemes as sc', 'sc.id', 's.scheme_id')
+    .select('s.id', 'sc.scheme_name', 's.instructions', "s.step_number")
+    .where('scheme_id', id)
+    .orderBy('s.step_number', 'asc');
 }
